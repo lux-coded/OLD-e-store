1c3aecbaf3c2d4f306b6165b01f3113ef1b0ee5f
lux-coded <selakluka@gmail.com>
2021-06-03 00:53:20 +0200

    Navbar icon properly displays cart number, created Cart page.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/logo.svg';
 import useStyles from './styles';
 
-const Navbar = (props) => {
+const Navbar = ({ totalItems }) => {
   const classes = useStyles();
-  
+
   return (
     <>
       <AppBar position='fixed' className={classes.appBar} color='inherit'>
@@ -19,7 +19,7 @@ const Navbar = (props) => {
           <div className={classes.grow} />
           <div className={classes.button}>
             <IconButton aria-label='Show cart items' color='inherit'>
-              <Badge badgeContent={2} color='secondary'>
+              <Badge badgeContent={totalItems} color='secondary'>
                 <ShoppingCart />
               </Badge>
             </IconButton>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+
 import Product from './Product/Product';
+import useStyles from './styles';
 
-const products = [
-  {id: 1, name: 'Notebook', description: 'Modern book for note taking.', price: '$10', image: 'https://images.unsplash.com/photo-1598620616655-7fce1a6fdf87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'},
-  {id: 2, name: 'Bag', description: 'Modern bag for all your carrying needs.', price: '$80', image: 'https://images.unsplash.com/photo-1546938576-6e6a64f317cc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'},
-]
+const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
 
-const Products = (props) => {
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify='center' spacing={4}>
         {
           products.map((product) => (
             <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-              <Product product={product}/>
+              <Product product={product} onAddToCart={onAddToCart}/>
             </Grid>
           ))
         }
